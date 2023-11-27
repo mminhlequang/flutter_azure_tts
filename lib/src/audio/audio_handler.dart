53ac3f946571098120da914ef3a0e121ed1aa039
Minh Minh <mminh.lequang@gmail.com>
2023-11-27 13:23:35 +0700

    -

--- lib/src/audio/audio_handler.dart
+++ lib/src/audio/audio_handler.dart
@@ -19,8 +19,14 @@ class AudioHandler {
         audioTypeHeader: AudioTypeHeader(audioFormat: params.audioFormat));
 
     try {
-      final ssml =
-          Ssml(voice: params.voice, text: params.text, speed: params.rate ?? 1);
+      final ssml = Ssml(
+        voice: params.voice,
+        text: params.text,
+        speed: params.rate ?? 1,
+        customInfos: params.customInfos,
+      );
+
+      print("ssml.buildSsml: ${ssml.buildSsml}");
 
       final response = await audioClient.post(Uri.parse(Endpoints.audio),
           body: ssml.buildSsml);
